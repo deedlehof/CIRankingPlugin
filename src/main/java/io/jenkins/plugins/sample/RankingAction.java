d197b6262d056d70079bd709a4880b5c6f5fa01c
deedlehof <tannerco97@gmail.com>
2020-04-12 02:26:36 +0000

    Added textbox and confirm button to page

--- src/main/java/io/jenkins/plugins/sample/RankingAction.java
+++ src/main/java/io/jenkins/plugins/sample/RankingAction.java
@@ -1,26 +1,34 @@
 package io.jenkins.plugins.sample;
 
 import hudson.Extension;
+import hudson.model.AbstractDescribableImpl;
 import hudson.model.Action;
+import hudson.model.Descriptor;
+import hudson.model.Job;
 import hudson.model.Project;
+import hudson.util.FormValidation;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import javax.servlet.ServletException;
+import javax.ws.rs.POST;
 import jenkins.model.TransientActionFactory;
-
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.QueryParameter;
 
 /**
 * HelloWorldAction is responsible for serving
 * data to the plugin front-end.
 */
-public class RankingAction implements Action {
+public class RankingAction extends AbstractDescribableImpl<RankingAction> implements Action {
 
   private transient FileComparator fc;
   private transient Map<String, Double> matchedFiles;
 
   private String codeLocation;
-  private String bugReport;
+  private String report;
 
   private Project project;
 
@@ -33,15 +41,15 @@ public class RankingAction implements Action {
     this.project = project;
 
     this.codeLocation = "/home/tanner/School/Project/Lang/src/main"; //TODO DELETE
-    this.bugReport = "  NumberUtils.createLong() does not handle hex numbers, but createInteger() handles hex and octal. This seems odd.  NumberUtils.createNumber() assumes that hex numbers can only be Integer. Again, why not handle bigger Hex numbers?  ==  It is trivial to fix createLong() - just use Long.decode() instead of valueOf(). It's not clear why this was not done originally - the decode() method was added to both Integer and Long in Java 1.2.  Fixing createNumber() is also fairly easy - if the hex string has more than 8 digits, use Long.  Should we allow for leading zeros in an Integer? If not, the length check is trivial.";
+    this.report = "  NumberUtils.createLong() does not handle hex numbers, but createInteger() handles hex and octal. This seems odd.  NumberUtils.createNumber() assumes that hex numbers can only be Integer. Again, why not handle bigger Hex numbers?  ==  It is trivial to fix createLong() - just use Long.decode() instead of valueOf(). It's not clear why this was not done originally - the decode() method was added to both Integer and Long in Java 1.2.  Fixing createNumber() is also fairly easy - if the hex string has more than 8 digits, use Long.  Should we allow for leading zeros in an Integer? If not, the length check is trivial.";
     /*
     this.codeLocation = codeLocation;
-    this.bugReport = bugReport;
+    this.report = report;
     */
     fc = new FileComparator("Lang");
     fc.trackDirectory(codeLocation);
 
-    matchedFiles = fc.compare(bugReport, 5);
+    matchedFiles = fc.compare(report, 5);
     System.err.println("=======TOP MATCHING FILES=========");
     for (Map.Entry<String, Double> entry: matchedFiles.entrySet()) {
       System.out.println(entry.getValue() + " --- " + entry.getKey());
@@ -74,7 +82,7 @@ public class RankingAction implements Action {
 
   @Override
   public String getUrlName() {
-    return "bugranking";
+    return "RankingAction";
   }
 
   @Extension
@@ -90,5 +98,17 @@ public class RankingAction implements Action {
     public Collection<? extends Action> createFor(Project project) {
       return Collections.singleton(new RankingAction(project));
     } 
+  } 
+
+  @Extension
+  public static class DescriptorImpl extends Descriptor<RankingAction> {
+
+    //@POST
+    public FormValidation doCalculateRanking(@QueryParameter("bugReport") final String bugReport, 
+	@AncestorInPath Job job) throws IOException, ServletException {
+      //report = bugReport;
+      System.err.println("CALLED DO CALCULATE");
+      return FormValidation.ok("Success");
+    }
   } 
 }
